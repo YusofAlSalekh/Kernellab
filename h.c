b73d9f8af8c9fc8a0441a1719c660e21681286ed
eight-dollars <noggano060571noggano@mail.ru>
2021-11-12 20:20:24 +0300

    S02

--- h.c
+++ h.c
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: GPL-2.0
-/* hello.c */
+/* hello_param.c */
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+MODULE_LICENSE("GPL");
+static char *whom = "MAI";
+static char *feeling = "satisfied";
+module_param(whom, charp, 0644);
+MODULE_PARM_DESC(whom, "Recipient of the hello message");
+static int howmany = 1;
+module_param(howmany, int, 0644);
+MODULE_PARM_DESC(howmany, "Number of greetings");
 static int __init hello_init(void)
 {
-	pr_alert("Hello MAI.\n");
-	return 0;
+int i;
+for (i = 0; i < howmany; i++)
+pr_alert("(%d) Hello,%s %s\n", i, whom, feeling);
+return 0;
 }
 static void __exit hello_exit(void)
 {
-	pr_alert("Bye MAI\n");
+pr_alert("Goodbye, %s\n", whom);
 }
 module_init(hello_init);
 module_exit(hello_exit);
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("lab1");
-MODULE_AUTHOR("Yusof");
 
